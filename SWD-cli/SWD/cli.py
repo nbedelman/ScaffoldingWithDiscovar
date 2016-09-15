033a104186cdb1337c226c8799ef539df4ba187b
nbedelman <nedelman@g.harvard.edu>
2016-09-15 15:36:50 -0400

    trying to get cli to work

--- SWD-cli/SWD/cli.py
+++ SWD-cli/SWD/cli.py
@@ -1,19 +1,19 @@
 """
 SWD
 Usage:
-  SWD hello
+  SWD hello <name> 
   SWD lastAlign
   SWD mafToSwbed
-  SWD reOrderChroms
+  SWD reOrderScaffolds <d> <m> <fo> <fn>
   SWD -h | --help
   SWD --version
 Options:
+  <d>                               Directory with contig bed files
+  <m>                               Original genome map file (AGP)
+  <fo>                              Fasta file of reference genome
+  <fn>                              Fasta file of new assembly
   -h --help                         Show this screen.
   --version                         Show version.
-Examples:
-  SWD lastAlign -r <REF> -q <QUERY> -l <LAST OPTIONS> -o <FOLDER FOR OUTPUT>
-  SWD mafToSwbed -m <MAF FILE> -o <FOLDER FOR OUTPUT>
-  SWD reOrderChroms
 Help:
   For help using this tool, please open an issue on the Github repository:
   https://github.com/nbedelman/ScaffoldingWithDiscovar
@@ -35,7 +35,7 @@ def main():
     # Here we'll try to dynamically match the command the user is trying to run
     # with a pre-defined command class we've already created.
     for k, v in options.iteritems():
-        if hasattr(commands, k) and v:
+        if hasattr(commands, k):
             module = getattr(commands, k)
             commands = getmembers(module, isclass)
             command = [command[1] for command in commands if command[0] != 'Base'][0]
