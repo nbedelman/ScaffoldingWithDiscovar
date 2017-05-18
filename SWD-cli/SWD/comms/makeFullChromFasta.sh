15e755178c7ae766df2519e39bb8e4a5be6c275c
nbedelman <nedelman@g.harvard.edu>
2017-05-18 12:47:58 -0400

    added script to convert chromosomal output to full genome fasta

--- SWD-cli/SWD/comms/makeFullChromFasta.sh
+++ SWD-cli/SWD/comms/makeFullChromFasta.sh
@@ -5,6 +5,8 @@ outputFile=$2
 
 for f in *discoOrder.fasta
 do header=$(basename $f .fasta)\_$round
-echo $header >> $outputFile
-grep -v ">" $f >> $outputFile
+echo ">"$header >> $outputFile
+grep -v ">" $f | tr -d '\n'|awk 1 >> $outputFile
 done
+python ~/Documents/Mallet_Lab/referenceScaffolding/ScaffoldingWithDiscovar/SWD-cli/SWD/comms/formatFasta.py $outputFile
+#mv $outputFile\_tmp $outputFile
