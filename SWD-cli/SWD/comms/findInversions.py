d27bfea8b3990e8f3fc68bf0537d647a96ebaca6
nbedelman <nedelman@g.harvard.edu>
2018-06-20 16:10:58 -0400

    adjusted inversion scripts for cluster use

--- SWD-cli/SWD/comms/findInversions.py
+++ SWD-cli/SWD/comms/findInversions.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 #findInversions.py
 #look for inversions between a DISCOVAR genome and a reference genome. Only consider those deletions found WITHIN a single reference scaffold.
 import sys
@@ -10,7 +12,7 @@ import string
 import copy
 from operator import itemgetter
 
-sys.path.insert(0, '/Users/nbedelman/Documents/Mallet_Lab/referenceScaffolding/ScaffoldingWithDiscovar/SWD-cli/SWD/comms/')
+#sys.path.insert(0, '/Users/nbedelman/Documents/Mallet_Lab/referenceScaffolding/ScaffoldingWithDiscovar/SWD-cli/SWD/comms/')
 
 from ScaffoldClass import *
 from ContigClass import *
@@ -23,10 +25,11 @@ from ErrorClasses import *
 from reOrderScaffolds import read_csv, write_csv, groupPiecesByChromosome, makeChromosomes, readScaffold, combineSegments, cullSegments, readAllContigs
 
 
-#agpBedFile="/Users/nbedelman/Documents/Mallet_Lab/referenceScaffolding/Hmel3/Hmel2_ordered_simpleName.fa.bed"
-agpBedFile="/Users/nbedelman/Documents/Mallet_Lab/18Genomes/Genomic_Analysis/correctScaffs_fixed.bed"
+agpBedFile=sys.argv[1]
+bedDirectory=sys.argv[2]
+outDir=sys.argv[3]
+
 scaffolds=readScaffold(agpBedFile)
-bedDirectory="/Users/nbedelman/Documents/Mallet_Lab/referenceScaffolding/inversionBeds"
 rawContigs=readAllContigs(bedDirectory,None)
 for contig in rawContigs:
     contig.findConnectors(scaffolds, "good")
@@ -39,4 +42,4 @@ for i in simpContigs:
     inv.findInversion()
     if inv.isInversion:
         interted=inv
-        inv.outputBed(inv.getSegments(),'''/Users/nbedelman/Desktop/inversionCandidates/%s.bed''' % (inv.getContig().getName()))
+        inv.outputBed(inv.getSegments(),'''%s/%s.bed''' % (outDir,inv.getContig().getName()))
